d53baaf0f238f9cb1bd3a2af0525c899ca78faf8
Wafi <muhammadwafi.wm@gmail.com>
2025-12-07 19:52:34 +0900

    templateGrittyContrast added

--- ossimg/template.py
+++ ossimg/template.py
@@ -5,6 +5,11 @@
 
 
 #Gritty Contrast Template
-
+def apply_gritty_contrast(img: Image.Image) -> Image.Image:
+    img = adjust_sharpness(img, 2.50)
+    img = adjust_brightness(img, 0.90)
+    img = adjust_shadows(img, -0.20)
+    img = adjust_saturation(img, 0.80) 
+    return img
 
 #Pastel Matte Template
